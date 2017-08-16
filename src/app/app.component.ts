0dfb81e66ade76ee3effeb4c3f981e662026a5b6
vivek munjewar <developer-hyd@gmail.com>
2017-08-16 10:29:03 +0530

    ngOnInit event added to fetch heroes list from injected service layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Hero} from './hero/hero';
+import { HeroService } from './service/hero.service';
 
 
 @Component({
 
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
-	selector: 'app-root'
+	selector: 'app-root',
+  providers: [HeroService]
 
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 	title = 'Tour of Heroes';
 	selectedHero: Hero;
-  heroes = HEROES;
+  heroes : Hero[];
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	};
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 	
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 
 
